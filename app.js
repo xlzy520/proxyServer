8816016c5d72c9c1d7729655fc3b7cad0de3e735
xlzy <1017866168@qq.com>
2022-04-18 11:56:35 +0800

    feat: vercel测试

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const https = require('https');
+// const cookieParser = require('cookie-parser');
+
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.all('*', (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.status(200);
+  }
+  next();
+});
+
+axios.default.defaults.baseURL =
+  'https://masterep.sampras.cn/index.php/factory-api/';
+const defaultHeaders = {
+  authorization: 'Basic bWFzdGVyOnBNUkFnQWpNZTFiSW5qMUI=',
+};
+axios.default.defaults.httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
+const axiosGet = (req) => {
+  return axios.get(req.path, {
+    params: req.query,
+    headers: {
+      ...defaultHeaders,
+      ...req.headers,
+    }
+  });
+};
+const axiosPost = (req) => {
+  return axios.post(req.path, req.body, {
+    headers: {
+      ...defaultHeaders,
+      ...req.headers,
+    }
+  });
+};
+
+app.post('*', async (req, res) => {
+  const result = await axiosPost(req);
+  res.send(result.data);
+  res.end();
+});
+
+app.get('*', async (req, res) => {
+  const result = await axiosGet(req);
+  res.send(result.data);
+  res.end();
+});
+
+module.exports = app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const analyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+
+const useAnalyzer = process.env.use_analyzer === 'true';
+const plugins = useAnalyzer ? [new analyzer()] : [];
 module.exports = {
-  entry: './sls.js', // 单入口
-  mode: "production",
+  entry: './bin/www', // 单入口
+  mode: 'production',
   output: {
-    filename: 'pro.js',
-    path: path.resolve(__dirname,'dist'),
+    filename: 'dist.js',
+    path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'commonjs2',
   },
-  target: 'node' // 这是最关键的
+  target: 'node', // 这是最关键的
+  plugins,
 };
